f93f6257892f325d8eecae0549d7af307028d5cf
TAHMID2K22 <chowdhurytahmid20@gmail.com>
2025-11-14 12:10:46 +0600

    adding header

--- src/main/java/com/example/mahin_2207109__cvbuilder/CV_Builder.java
+++ src/main/java/com/example/mahin_2207109__cvbuilder/CV_Builder.java
@@ -7,11 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class CV_Builder extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(CV_Builder.class.getResource("Home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();

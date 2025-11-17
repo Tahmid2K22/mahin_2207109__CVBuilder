869cb7103ec1373e8d5937e8ded1cbd69a30104c
TAHMID2K22 <chowdhurytahmid20@gmail.com>
2025-11-17 23:57:23 +0600

    Adding controller for home

--- src/main/java/com/example/mahin_2207109__cvbuilder/CV_Builder.java
+++ src/main/java/com/example/mahin_2207109__cvbuilder/CV_Builder.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class CV_Builder extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CV_Builder.class.getResource("CV_show.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CV_Builder.class.getResource("Home.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ src/main/java/com/example/mahin_2207109__cvbuilder/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.mahin_2207109__cvbuilder;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+public class HomeController {
+
+    @FXML
+    void next_input_scene(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("CV_Input.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
